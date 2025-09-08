125c718775f929442e51466c258486e3d18a8c70
Dielo137 <diego.lopez85@inacap.cl>
2025-09-08 18:53:34 -0300

    Module "Products" was created in NestJS.

--- distribuidora-api/src/app.module.ts
+++ distribuidora-api/src/app.module.ts
@@ -5,13 +5,15 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, 
     }),
-     MongooseModule.forRoot(process.env.DATABASE_URL || 'mongodb://localhost/fallback-db-error'), 
+     MongooseModule.forRoot(process.env.DATABASE_URL || 'mongodb://localhost/fallback-db-error'),
+     ProductsModule, 
   ],
   controllers: [AppController],
   providers: [AppService],
