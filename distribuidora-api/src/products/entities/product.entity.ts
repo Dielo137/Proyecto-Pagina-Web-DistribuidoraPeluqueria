1b97afffa337e9cfff9e23fc7fa97fe1890b8ce5
Dielo137 <diego.lopez85@inacap.cl>
2025-09-09 16:31:09 -0300

    The schema  of the products was defined , so mongodb/moongose can use it

--- distribuidora-api/src/products/entities/product.entity.ts
+++ distribuidora-api/src/products/entities/product.entity.ts
@@ -1,1 +1,30 @@
-export class Product {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type ProductDocument = HydratedDocument<Product>;
+
+@Schema({ timestamps: true }) 
+export class Product {
+  @Prop({ required: true, unique: true })
+  sku: string;
+
+  @Prop({ required: true })
+  nombre: string;
+
+  @Prop()
+  descripcion: string;
+
+  @Prop({ required: true, type: Number, min: 0 })
+  precio: number;
+
+  @Prop()
+  imagenURL: string;
+
+  @Prop({ required: true, type: Number, min: 0, default: 0 })
+  stock: number;
+
+  @Prop() 
+  categoriaId: string;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product); 
